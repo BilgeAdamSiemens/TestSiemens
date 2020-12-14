7e4ce174dfc3db3ec1169725ee95c0fb95016d53
omur <omurucum@gmail.com>
2020-12-14 09:50:24 +0300

    UnitTest 1. Odev Cozumu

---
+++ TestSiemens/TestSiemens.Test/CezaPuaniTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+namespace TestSiemens.Test
+{
+    [TestFixture]
+    public class CezaPuaniTest
+    {
+        private CezaPuani _HSP;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //Arrange
+            _HSP = new CezaPuani();
+        }
+
+        [Test]
+        [TestCase(-10)]
+        [TestCase(301)]
+        public void CezaPuaniHesaplama_SpeedIsOutOfRange_ThrowArgumentOutOfRangeException
+            (int speed)
+        {
+            //Assert
+            Assert.That(
+                () => _HSP.CezaPuaniHesaplama(speed),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>()
+            );
+        }
+
+        [Test]
+        [TestCase(0,0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 0)]
+        [TestCase(255, 37)]
+        public void CezaPuaniHesaplama_WhenCalled_ReturnPenaltyPoint
+            (int speed, int expectedResult)
+        {
+            //Act
+            var penaltyPoint = _HSP.CezaPuaniHesaplama(speed);
+
+            Assert.That(penaltyPoint, Is.GreaterThanOrEqualTo(expectedResult));
+        }
+
+    }   
+}
---
+++ TestSiemens/TestSiemens/CezaPuani.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace TestSiemens
+{
+    public class CezaPuani
+    {
+        private const int HizLimit = 65;
+        private const int HizUstLimit = 300;
+
+        public int CezaPuaniHesaplama(int hiz)
+        {
+            if (hiz < 0 || hiz > HizUstLimit)
+                throw new ArgumentOutOfRangeException();
+
+            if (hiz <= HizLimit) return 0;
+
+            const int KmIcinCezaPuani = 5;
+            var cezaPuani = (hiz - HizLimit) / KmIcinCezaPuani;
+                            
+            return cezaPuani;
+        }
+    }
+}
